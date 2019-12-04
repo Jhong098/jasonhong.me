5fcf5d34a6c72b197ab3bcd45767fe0002852c29
Jhong098 <Jhong098@gmail.com>
2019-12-04 00:01:20 -0800

    ignore serviceworker for eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="App">Hello</div>;
 };
 
 export default App;
