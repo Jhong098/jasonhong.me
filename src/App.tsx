6317440f0fd97d4a1d91423de4526e0e3a58aad8
Jason Hong <jhong098@gmail.com>
2020-03-12 22:16:27 -0400

    starting page transitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,40 @@
 import React from "react";
 
 import Logo from "Components/Logo";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation
+} from "react-router-dom";
 
 import "./App.scss";
 import Landing from "Pages/Landing";
 import Links from "Components/Links";
 import Experiences from "Pages/Experiences";
 import ExperienceDetails from "Pages/Experiences/ExperienceDetails";
+import { AnimatePresence } from "framer-motion";
 
 const App: React.FC = () => {
+  const location = useLocation();
   return (
     <div className="App">
       {/* <canvas id="cursor" style={{ height: "100%", width: "100%" }} /> */}
       <Links />
       <Router>
-        <Switch>
-          <Route exact path="/">
-            <Landing />
-          </Route>
-          <Route exact path="/experience">
-            <Experiences />
-          </Route>
-          <Route path="/experience/:number">
-            <ExperienceDetails />
-          </Route>
-        </Switch>
+        <AnimatePresence>
+          <Switch location={location} key={location.pathname}>
+            <Route exact path="/">
+              <Landing />
+            </Route>
+            <Route exact path="/experience">
+              <Experiences />
+            </Route>
+            <Route path="/experience/:number">
+              <ExperienceDetails />
+            </Route>
+          </Switch>
+        </AnimatePresence>
         <Logo />
       </Router>
     </div>
