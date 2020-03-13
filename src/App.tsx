9a210593ffda7c2c59fc9b3d1c3689899119339c
Jason Hong <jhong098@gmail.com>
2020-03-12 22:30:17 -0400

    starting experience details page

--- src/App.tsx
+++ src/App.tsx
@@ -15,26 +15,26 @@ import Experiences from "Pages/Experiences";
 import ExperienceDetails from "Pages/Experiences/ExperienceDetails";
 import { AnimatePresence } from "framer-motion";
 
-const App: React.FC = () => {
-  const location = useLocation();
+const App = () => {
+  // const location = useLocation();
   return (
     <div className="App">
       {/* <canvas id="cursor" style={{ height: "100%", width: "100%" }} /> */}
       <Links />
       <Router>
-        <AnimatePresence>
-          <Switch location={location} key={location.pathname}>
-            <Route exact path="/">
-              <Landing />
-            </Route>
-            <Route exact path="/experience">
-              <Experiences />
-            </Route>
-            <Route path="/experience/:number">
-              <ExperienceDetails />
-            </Route>
-          </Switch>
-        </AnimatePresence>
+        {/* <AnimatePresence> */}
+        <Switch>
+          <Route exact path="/">
+            <Landing />
+          </Route>
+          <Route exact path="/experience">
+            <Experiences />
+          </Route>
+          <Route path="/experience/:number">
+            <ExperienceDetails />
+          </Route>
+        </Switch>
+        {/* </AnimatePresence> */}
         <Logo />
       </Router>
     </div>
--- src/Pages/Experiences/ExperienceDetails.tsx
+++ src/Pages/Experiences/ExperienceDetails.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { experiencesList } from "copy";
+import "./Experiences.scss";
 
 const ExperienceDetails = () => {
   const { number } = useParams();
-  return <div>{number}</div>;
+  const { title, img, desc } = experiencesList[number || 0];
+  return (
+    <div id="experience-details">
+      <div className="bg-img" style={{ backgroundImage: `url(${img})` }}></div>
+      <h1>{title}</h1>
+    </div>
+  );
 };
 
 export default ExperienceDetails;
