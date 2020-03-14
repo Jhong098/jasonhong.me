fa9191ddc36aa97757a81eeeb308015e2125141e
Jason Hong <jhong098@gmail.com>
2020-03-14 01:01:31 -0400

    start travel page

--- src/Pages/Travel/index.tsx
+++ src/Pages/Travel/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Cards from "Components/Cards";
+
+import { hk1, hk2, hk3, hk4, hk5, hk6 } from "static";
 
 const Travel = () => {
-  return <div>Travel</div>;
+  return (
+    <div>
+      <Cards images={[hk1, hk2, hk3, hk4, hk5, hk6]} />
+    </div>
+  );
 };
 
 export default Travel;
--- src/static/index.ts
+++ src/static/index.ts
@@ -11,3 +11,11 @@ export { default as resume } from "./icons/file.svg";
 export { default as linkedin } from "./icons/linkedin.svg";
 export { default as github } from "./icons/github.svg";
 export { default as mail } from "./icons/mail.svg";
+
+/* Travel */
+export { default as hk1 } from "./img/travel/hongkong_1.jpg";
+export { default as hk2 } from "./img/travel/hongkong_2.jpg";
+export { default as hk3 } from "./img/travel/hongkong_3.jpg";
+export { default as hk4 } from "./img/travel/hongkong_4.jpg";
+export { default as hk5 } from "./img/travel/hongkong_5.jpg";
+export { default as hk6 } from "./img/travel/hongkong_6.jpg";
