4e5f59e58abc23cc9c32ec29bd0e33b0cfdd4f49
Jhong098 <Jhong098@gmail.com>
2020-11-22 01:27:56 -0800

    fix spotify icon and pass in images count to grid container

--- components/NowPlaying.js
+++ components/NowPlaying.js
@@ -5,7 +5,6 @@ import {
   Stack,
   Image,
   Text,
-  Icon,
   Skeleton,
   useColorMode
 } from '@chakra-ui/react';
@@ -13,6 +12,7 @@ import {
 import fetcher from 'lib/fetcher';
 
 import { borderColor } from 'styles/darkMode';
+import { SpotifyIcon } from './CustomIcons';
 
 const NowPlaying = () => {
   const { data } = useSWR('/api/now-playing', fetcher);
@@ -69,7 +69,7 @@ const NowPlaying = () => {
           {data && (data?.artist || 'Spotify')}
         </Text>
       </Stack>
-      <Icon name="spotify" ml="auto" mt={1} />
+      <SpotifyIcon ml="auto" mt={1} />
     </Box>
   );
 };
