c0639df6fec9e31cd85c14831bc8015243311a90
Jhong098 <jhong098@gmail.com>
2021-03-11 21:39:34 -0800

    remove next-google-fonts

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
-import GoogleFonts from 'next-google-fonts';
 import { ColorModeScript } from '@chakra-ui/react';
 import theme from 'styles/theme';
 
