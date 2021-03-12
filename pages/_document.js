190a794bc7ffb80135743c2c1b75acab7d4de599
Jhong098 <jhong098@gmail.com>
2021-03-11 20:18:50 -0800

    update deps

--- pages/_document.js
+++ pages/_document.js
@@ -7,8 +7,8 @@ class MyDocument extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" />
         <Head>
+          
           <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
           <link href="/static/favicons/site.webmanifest" rel="manifest" />
           <link
@@ -33,6 +33,7 @@ class MyDocument extends NextDocument {
             sizes="16x16"
             type="image/png"
           />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" />
         </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
