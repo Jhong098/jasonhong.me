c267fd1cd0bc1d4a10ecef3c6326c0a73eb3e079
Jhong098 <jhong098@gmail.com>
2026-03-29 23:41:17 -0700

    copy and font fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://jasonhong.me',
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
     resolve: {
